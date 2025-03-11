1f9f813cf677d720e23fc5263fc368f51f6792ea
rmesend <55415644+rmesend@users.noreply.github.com>
2025-03-11 02:44:10 +0000

    Deploying to gh-pages from @ rmesend/rmesend.github.io@ceb179063af37e7a7de21af608d3e6a6476fac75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -446,7 +446,12 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "books-the-godfather",
+    },{id: "books-",
+          title: '',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/Tecno-Feudalismo/";
+            },},{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
