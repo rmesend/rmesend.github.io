31a839798df3228d5c4177fd8bbd90caef81e13a
rmesend <55415644+rmesend@users.noreply.github.com>
2025-03-12 00:07:15 +0000

    Deploying to gh-pages from @ rmesend/rmesend.github.io@bbbf1ce528254f56e3b1bfcb5d71f65add361fda 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -121,11 +121,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/Tecno-Feudalismo/";
-            },},{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
